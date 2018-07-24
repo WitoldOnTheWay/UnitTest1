10f6add36a7297efe4e41a1b79a971cf88ae77a4
Witold Skobrtal <witold_sko@op.pl>
2018-07-24 21:28:41 +0200

    First unit test on calculator methods

---
+++ KalkulatorTests/KalkulatorTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CountingApp;
+
+namespace KalkulatorTests
+{
+    [TestClass]
+    public class KalkulatorTests
+    {
+        [TestMethod]
+        public void AdditionMethodTest()
+        {
+           int expected = 4;
+            Kalkulator kalkulator = new Kalkulator();
+            int actual=kalkulator.Addition(2,2);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void SubstractionMethodTest()
+        {
+            int expected = 3;
+            Kalkulator kalkulator = new Kalkulator();
+            int actual = kalkulator.Substraction(5, 2);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void MultiplicationMethodTest()
+        {
+            int expected = 25;
+            Kalkulator kalkulator = new Kalkulator();
+            int actual = kalkulator.Multiplication(5, 5);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void DivisionMethodTest()
+        {
+            int expected = 4;
+            Kalkulator kalkulator = new Kalkulator();
+            int actual = kalkulator.Division(12, 3);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(System.DivideByZeroException))]
+        public void DivisionByZeroMethodTest()
+        {
+            Kalkulator kalkulator = new Kalkulator();
+            int actual = kalkulator.Division(12, 0);
+        }
+}
+}
---
+++ KalkulatorTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KalkulatorTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KalkulatorTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("22a837b4-01b9-492d-8a91-53a1816b16d2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
